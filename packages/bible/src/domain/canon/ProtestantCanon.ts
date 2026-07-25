46119604ba4858f0fd02c8c807c0274b199267a7
johann-han <johann@woordvanlewe.co.za>
2026-07-25 21:44:20 +0200

    new file:   packages/bible/src/domain/aggregates/Canon.ts 	modified:   packages/bible/src/domain/canon/ProtestantCanon.ts 	new file:   packages/bible/src/domain/canon/createBook.ts 	new file:   packages/bible/src/domain/services/BibleReferenceResolver.ts

--- packages/bible/src/domain/canon/ProtestantCanon.ts
+++ packages/bible/src/domain/canon/ProtestantCanon.ts
@@ -1,3 +1,38 @@
-import { BibleBook } from "../entities/index.js";
+import { BibleDivision } from "../classification/index.js";
+import { Testament } from "../classification/index.js";
 
-export const PROTESTANT_CANON: readonly BibleBook[] = [];
+import { createBook } from "./createBook.js";
+
+export const PROTESTANT_CANON = [
+
+    createBook({
+        id: "GEN",
+        canonicalName: "Genesis",
+        shortName: "Genesis",
+        abbreviation: "Gen",
+        testament: Testament.Old,
+        division: BibleDivision.Law,
+        chapterCount: 50,
+    }),
+
+    createBook({
+        id: "EXO",
+        canonicalName: "Exodus",
+        shortName: "Exodus",
+        abbreviation: "Ex",
+        testament: Testament.Old,
+        division: BibleDivision.Law,
+        chapterCount: 40,
+    }),
+
+    createBook({
+        id: "LEV",
+        canonicalName: "Leviticus",
+        shortName: "Leviticus",
+        abbreviation: "Lev",
+        testament: Testament.Old,
+        division: BibleDivision.Law,
+        chapterCount: 27,
+    }),
+
+];
