7b0ca662a48b422711b58b3ebc0854d69f5048ac
johann-han <johann@woordvanlewe.co.za>
2026-07-26 20:45:04 +0200

    modified:   packages/bible/src/domain/canon/ProtestantCanon.ts

--- packages/bible/src/domain/canon/ProtestantCanon.ts
+++ packages/bible/src/domain/canon/ProtestantCanon.ts
@@ -1,41 +1,5 @@
-import { BibleDivision } from "../classification/index.js";
-import { Testament } from "../classification/index.js";
-
-import { createBook } from "../factories/createBook.js";
+import { pentateuch } from "./books/index.js";
 
 export const PROTESTANT_CANON = [
-
-    createBook({
-        id: "GEN",
-        canonicalName: "Genesis",
-        shortName: "Genesis",
-        abbreviation: "Gen",
-        canonicalOrder: 1,
-        testament: Testament.Old,
-        division: BibleDivision.Law,
-        chapterCount: 50,
-    }),
-
-    createBook({
-        id: "EXO",
-        canonicalName: "Exodus",
-        shortName: "Exodus",
-        abbreviation: "Ex",
-        canonicalOrder: 2,
-        testament: Testament.Old,
-        division: BibleDivision.Law,
-        chapterCount: 40,
-    }),
-
-    createBook({
-        id: "LEV",
-        canonicalName: "Leviticus",
-        shortName: "Leviticus",
-        abbreviation: "Lev",
-        canonicalOrder: 1,
-        testament: Testament.Old,
-        division: BibleDivision.Law,
-        chapterCount: 27,
-    }),
-
-];
+    ...pentateuch,
+] as const;
