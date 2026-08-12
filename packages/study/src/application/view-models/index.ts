f35df5d7b70015a1982b4968eb2aef723c04cfd7
Johann Hanekom <johann@woordvanlewe.co.za>
2026-08-12 12:58:20 +0200

    feat(study): export observation view model

--- packages/study/src/application/view-models/index.ts
+++ packages/study/src/application/view-models/index.ts
@@ -1,3 +1,4 @@
 export * from "./ObservationQuestionViewModel.js";
 export * from "./ConnectingWordViewModel.js";
+export * from "./ObservationViewModel.js";
 export * from "./ObservationWorkspaceViewModel.js";
