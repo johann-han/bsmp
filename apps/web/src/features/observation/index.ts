48112204d8fd04a226d23187b3ffdb6ad57cda52
Johann Hanekom <johann@woordvanlewe.co.za>
2026-08-12 15:21:36 +0200

    feat(web): export interpretation workspace components

--- apps/web/src/features/observation/index.ts
+++ apps/web/src/features/observation/index.ts
@@ -1,4 +1,6 @@
 export * from "./ObservationWorkspace";
 export * from "./ObservationComposer";
 export * from "./ObservationHistory";
+export * from "./InterpretationComposer";
+export * from "./InterpretationHistory";
 export * from "./StudyPassage";
