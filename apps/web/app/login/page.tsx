83e017b003d5b10942f61a33e250ae9114f1976e
Johann Hanekom <johann@woordvanlewe.co.za>
2026-08-12 13:37:24 +0200

    feat(web): add login route

---
+++ apps/web/app/login/page.tsx
@@ -0,0 +1,9 @@
+import { AuthForm } from "../../src/features/auth/index.js";
+
+export default function LoginPage() {
+    return (
+        <main style={{ padding: 32 }}>
+            <AuthForm />
+        </main>
+    );
+}
