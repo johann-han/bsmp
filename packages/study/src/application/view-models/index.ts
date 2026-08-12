a95a2edd1d16239cba6975d82a7f0cf360f1468a
Johann Hanekom <johann@woordvanlewe.co.za>
2026-08-12 15:31:09 +0200

    feat(study): export evidence view model

--- packages/study/src/application/view-models/index.ts
+++ packages/study/src/application/view-models/index.ts
@@ -1,5 +1,6 @@
 export * from "./ObservationQuestionViewModel.js";
 export * from "./ConnectingWordViewModel.js";
 export * from "./ObservationViewModel.js";
+export * from "./EvidenceViewModel.js";
 export * from "./InterpretationViewModel.js";
 export * from "./ObservationWorkspaceViewModel.js";
