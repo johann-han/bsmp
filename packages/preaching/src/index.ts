cf3ee8d54ceaf926692b7dd8b16c7dad82eddf22
Johann Hanekom <johann@woordvanlewe.co.za>
2026-08-12 16:35:00 +0200

    feat(preaching): export sermon persistence service

--- packages/preaching/src/index.ts
+++ packages/preaching/src/index.ts
@@ -1,3 +1,4 @@
 export * from "./domain/ExpositorySermon.js";
 export * from "./domain/ExpositorySermonRepository.js";
 export * from "./application/CreateExpositorySermonFromStudy.js";
+export * from "./application/CreateAndSaveExpositorySermonFromStudy.js";
