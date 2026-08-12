d55d0d447b38209ffd43cb46bfa3feaa0284d541
Johann Hanekom <johann@woordvanlewe.co.za>
2026-08-12 14:14:52 +0200

    test(study): update observation fixture with verse reference

--- packages/study/src/test/createObservation.ts
+++ packages/study/src/test/createObservation.ts
@@ -1,18 +1,36 @@
+import {
+    BookCode,
+    ChapterNumber,
+    VerseNumber,
+    VerseReference,
+} from "@bsmp/bible";
+
 import { Observation } from "../domain/entities/Observation.js";
 
 import {
     ObservationId,
     ObservationStatement,
+    ObservationVerseReference,
 } from "../domain/value-objects/index.js";
 
 export function createObservation(
     statement = "Test Observation",
 ): Observation {
 
+    const verseReference =
+        VerseReference.create(
+            BookCode.from("JHN"),
+            ChapterNumber.of(15),
+            VerseNumber.from(1),
+        );
+
     return Observation.create(
         ObservationId.create(),
         ObservationStatement.from(
             statement,
+        ),
+        ObservationVerseReference.from(
+            verseReference,
         ),
     );
 
