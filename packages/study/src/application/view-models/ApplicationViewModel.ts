eaa05544594af6c310ec6f6d067c576a2963362b
Johann Hanekom <johann@woordvanlewe.co.za>
2026-08-12 15:40:04 +0200

    feat(study): add application view model

---
+++ packages/study/src/application/view-models/ApplicationViewModel.ts
@@ -0,0 +1,9 @@
+export interface ApplicationViewModel {
+    readonly id: string;
+    readonly interpretationId: string;
+    readonly principle: string;
+    readonly personal: string;
+    readonly ministry: string;
+    readonly action: string;
+    readonly createdAt: string;
+}
