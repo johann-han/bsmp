7f9c2424c053c13620656508e9c9ecd4c72986d9
Johann Hanekom <johann@woordvanlewe.co.za>
2026-08-12 13:36:37 +0200

    feat(web): bootstrap observation workspace from Supabase

---
+++ apps/web/src/lib/createSupabaseObservationWorkspace.ts
@@ -0,0 +1,32 @@
+import {
+    createObservationWorkspace,
+    createStudyPassage,
+    StudyId,
+    StudySession,
+    StudyTitle,
+} from "@bsmp/study";
+
+import { SupabaseStudyRepository } from "./SupabaseStudyRepository.js";
+
+export async function createSupabaseObservationWorkspace() {
+    const repository = new SupabaseStudyRepository();
+    const passageService = createStudyPassage();
+    const studies = await repository.findAll();
+
+    let study = studies[0];
+
+    if (!study) {
+        study = StudySession.create(
+            StudyId.create(),
+            StudyTitle.from("John 15 Observation Study"),
+            passageService.passageReference,
+        );
+
+        await repository.save(study);
+    }
+
+    return {
+        workspace: createObservationWorkspace(repository, study),
+        passageService,
+    };
+}
