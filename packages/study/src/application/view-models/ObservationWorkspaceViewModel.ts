bd8c1aa1f89b985dd92102d44060ed6eb7fcd262
Johann Hanekom <johann@woordvanlewe.co.za>
2026-08-12 12:58:26 +0200

    feat(study): expose observations in workspace view model

--- packages/study/src/application/view-models/ObservationWorkspaceViewModel.ts
+++ packages/study/src/application/view-models/ObservationWorkspaceViewModel.ts
@@ -1,7 +1,9 @@
 import type { ObservationQuestionViewModel } from "./ObservationQuestionViewModel.js";
 import type { ConnectingWordViewModel } from "./ConnectingWordViewModel.js";
+import type { ObservationViewModel } from "./ObservationViewModel.js";
 
 export interface ObservationWorkspaceViewModel {
     observationQuestions: readonly ObservationQuestionViewModel[];
     connectingWords: readonly ConnectingWordViewModel[];
+    observations: readonly ObservationViewModel[];
 }
