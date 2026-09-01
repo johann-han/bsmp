763e4e5370a06fdd2e3dde7be7838d0aef11dacd
Johann Hanekom <johann@woordvanlewe.co.za>
2026-09-01 10:33:47 +0200

    feat(preaching): export manuscript builder

--- packages/preaching/src/index.ts
+++ packages/preaching/src/index.ts
@@ -2,3 +2,4 @@ export * from "./domain/ExpositorySermon.js";
 export * from "./domain/ExpositorySermonRepository.js";
 export * from "./application/CreateExpositorySermonFromStudy.js";
 export * from "./application/CreateAndSaveExpositorySermonFromStudy.js";
+export * from "./application/BuildSermonManuscript.js";
