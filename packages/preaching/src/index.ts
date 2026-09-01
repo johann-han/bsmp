8a62609876e38d2e4013b03a11983f5337f87785
Johann Hanekom <johann@woordvanlewe.co.za>
2026-09-01 16:47:18 +0200

    feat(preaching): export sermon occurrence model

--- packages/preaching/src/index.ts
+++ packages/preaching/src/index.ts
@@ -1,5 +1,7 @@
 export * from "./domain/ExpositorySermon.js";
 export * from "./domain/ExpositorySermonRepository.js";
+export * from "./domain/SermonOccurrence.js";
+export * from "./domain/SermonOccurrenceRepository.js";
 export * from "./application/CreateExpositorySermonFromStudy.js";
 export * from "./application/CreateAndSaveExpositorySermonFromStudy.js";
 export * from "./application/BuildSermonManuscript.js";
