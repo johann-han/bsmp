9977601e89adb1cc03b8131d59e7b4dadf8ed84e
Johann Hanekom <johann@woordvanlewe.co.za>
2026-09-02 16:58:04 +0200

    feat(study): add biblical theology view model

---
+++ packages/study/src/application/view-models/BiblicalTheologyViewModel.ts
@@ -0,0 +1,7 @@
+export interface BiblicalTheologyViewModel {
+    readonly id: string;
+    readonly theme: string;
+    readonly synthesis: string;
+    readonly interpretationIds: readonly string[];
+    readonly createdAt: string;
+}
