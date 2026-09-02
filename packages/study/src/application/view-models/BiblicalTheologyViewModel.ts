48e87420ec0fe09ea24dccc5bf71e533b1eec541
Johann Hanekom <johann@woordvanlewe.co.za>
2026-09-02 17:03:42 +0200

    chore(study): remove duplicate biblical theology view model

--- packages/study/src/application/view-models/BiblicalTheologyViewModel.ts
+++
@@ -1,7 +0,0 @@
-export interface BiblicalTheologyViewModel {
-    readonly id: string;
-    readonly theme: string;
-    readonly synthesis: string;
-    readonly interpretationIds: readonly string[];
-    readonly createdAt: string;
-}
