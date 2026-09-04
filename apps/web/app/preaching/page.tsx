659dfe0404a815dea63bf61293299b894880f4ca
Johann Hanekom <johann@woordvanlewe.co.za>
2026-09-04 16:17:30 +0200

    feat(preaching): connect teaching plan to sermon preparation

--- apps/web/app/preaching/page.tsx
+++ apps/web/app/preaching/page.tsx
@@ -1,5 +1,6 @@
 import { SermonPreparationWorkspace } from "../../src/features/preaching/SermonPreparationWorkspace";
+import { SermonTeachingFoundationSection } from "../../src/features/preaching/SermonTeachingFoundationSection";
 
 export default function PreachingPage() {
-    return <SermonPreparationWorkspace />;
+    return <><SermonTeachingFoundationSection /><SermonPreparationWorkspace /></>;
 }
