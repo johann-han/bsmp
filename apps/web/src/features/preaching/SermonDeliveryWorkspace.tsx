5d28766283ef596d318bd4844041802875226dfc
Johann Hanekom <johann@woordvanlewe.co.za>
2026-09-07 10:12:52 +0200

    fix(web): restore delivery router import

--- apps/web/src/features/preaching/SermonDeliveryWorkspace.tsx
+++ apps/web/src/features/preaching/SermonDeliveryWorkspace.tsx
@@ -2,7 +2,8 @@
 
 import Link from "next/link";
 import { useEffect, useMemo, useState } from "react";
-import type { ExpositorySermon, SermonManuscriptSection } from "@bsmp/preaching";
+import { useRouter } from "next/navigation";
+import type { ExpositorySermon } from "@bsmp/preaching";
 import { StudyId } from "@bsmp/study";
 import { AppShell } from "@repo/ui";
 import { SupabaseStudyRepository } from "../../lib/SupabaseStudyRepository";
