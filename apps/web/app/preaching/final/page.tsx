4821e160bb4a90fe5f463be4f6d4c0e3cc655228
Johann Hanekom <johann@woordvanlewe.co.za>
2026-09-07 08:57:32 +0200

    feat(web): show final sermon readiness checklist

--- apps/web/app/preaching/final/page.tsx
+++ apps/web/app/preaching/final/page.tsx
@@ -1,6 +1,7 @@
 import { SermonFinalDraftWorkspace } from "../../../src/features/preaching/SermonFinalDraftWorkspace";
 import { SermonTeachingTraceability } from "../../../src/features/preaching/SermonTeachingTraceability";
 import { FinalSermonDraftMentorPanel } from "../../../src/features/preaching/FinalSermonDraftMentorPanel";
+import { FinalSermonDraftReadiness } from "../../../src/features/preaching/FinalSermonDraftReadiness";
 
 interface Props {
     searchParams: Promise<{ studyId?: string }>;
@@ -12,6 +13,7 @@ export default async function FinalSermonDraftPage({ searchParams }: Props) {
     return (
         <>
             <SermonTeachingTraceability studyId={studyId} />
+            <FinalSermonDraftReadiness studyId={studyId} />
             <SermonFinalDraftWorkspace studyId={studyId} />
             <FinalSermonDraftMentorPanel studyId={studyId} />
         </>
