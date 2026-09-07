cbb141cdbf41778829ed39520d07beaf4ddc5e12
Johann Hanekom <johann@woordvanlewe.co.za>
2026-09-07 18:04:43 +0200

    Remove duplicate Biblical Theology mentor panel

--- apps/web/app/biblical-theology/page.tsx
+++ apps/web/app/biblical-theology/page.tsx
@@ -1,7 +1,6 @@
 import { redirect } from "next/navigation";
 
 import { AppShell } from "@repo/ui";
-import { BiblicalTheologyMentorPanel } from "../../src/features/biblical-theology/BiblicalTheologyMentorPanel";
 import { BiblicalTheologyWorkspace } from "../../src/features/biblical-theology/BiblicalTheologyWorkspace";
 
 interface PageProps {
@@ -15,7 +14,6 @@ export default async function BiblicalTheologyPage({ searchParams }: PageProps) 
     return (
         <AppShell title="Biblical Theology">
             <BiblicalTheologyWorkspace studyId={studyId} />
-            <BiblicalTheologyMentorPanel studyId={studyId} />
         </AppShell>
     );
 }
