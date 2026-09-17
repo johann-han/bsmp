551c2841df87a9e45785199b01d4c6598bd8a38f
Johann Hanekom <johann@woordvanlewe.co.za>
2026-09-17 18:22:10 +0200

    feat(billing): add subscription settings route

---
+++ apps/web/app/settings/subscription/page.tsx
@@ -0,0 +1,5 @@
+import { SubscriptionWorkspace } from "../../../src/features/settings/SubscriptionWorkspace";
+
+export default function SubscriptionPage() {
+    return <SubscriptionWorkspace />;
+}
